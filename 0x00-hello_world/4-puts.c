a0c675e3d1d4a5cbecebf87a502930a09fdb7fac
MarvelousMarvin7 <marvinagyei3@gmail.com>
2022-12-08 16:13:27 -0800

    C-Hello,

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *Output a sentence
+ *
+ *return 0 after success
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
+}
