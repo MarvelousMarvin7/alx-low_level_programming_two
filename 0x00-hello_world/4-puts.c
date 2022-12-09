4ffdf907b4a7fccc4b4d8c5350edf85684857514
MarvelousMarvin7 <marvinagyei3@gmail.com>
2022-12-08 16:22:25 -0800

    C-Hello,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *Output a sentence
+ *main - Output a sentence
  *
- *return 0 after success
+ *Return: 0 after success
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
