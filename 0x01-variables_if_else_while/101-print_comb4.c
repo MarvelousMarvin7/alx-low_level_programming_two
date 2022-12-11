6fad9a867f4c6ddef68771e207c27e1e996e31b6
MarvelousMarvin7 <marvinagyei3@gmail.com>
2022-12-11 15:14:00 -0800

    C-variables,if,else,while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - prints out all the numbers between 00 and 99
- * with no two digits being the same
+ * main - prints out all the numbers between 012 and 789
+ * with no three digits being the same
  * Return: 0
  */
 int main(void)
 {
 int digit1;
 int digit2;
+int digit3;
+
 for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
 {
+for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+{
 putchar(digit1 + '0');
 putchar(digit2 + '0');
-if (digit1 + digit2 < 17)
+putchar(digit3 + '0');
+if (digit1 + digit2 + digit3 < 24)
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
