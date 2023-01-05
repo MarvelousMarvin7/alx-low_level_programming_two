012e3dc7f7dcde2456a25ced24d160c92cb3c1ea
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-05 11:08:44 -0800

    C-Pointers and arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,8 @@
 
 /**
  * print_array - check the code for
- * @a, @n: variables to be used
+ * @a: variable or poiinter to call
+ * @n: number of element in the array
  * Return: Always 0.
  */
 void print_array(int *a, int n)
