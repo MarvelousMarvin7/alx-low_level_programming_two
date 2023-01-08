0ebd01ef3d88919a297a0ddb0827c9a3bcece381
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-07 16:50:41 -0800

    C-More pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk -  locates the first occurrence in a string
+ * @s: pointer of characters
+ * @accept: characters to match
+ * Return: s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, w;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (w = 0; accept[w]; w++)
+		{
+			if (s[i] == accept[w])
+			{
+				break;
+			}
+		}
+		if (accept[w])
+			break;
+	}
+	return (s + i);
+	return (0);
+}
