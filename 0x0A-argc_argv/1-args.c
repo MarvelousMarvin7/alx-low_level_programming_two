7e1f166abeddee1b8fa747aa2419da2ee229a367
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-09 09:23:43 -0800

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: counts number of argument in the command line
+ * @argv: output number of array of strings on the command line
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
