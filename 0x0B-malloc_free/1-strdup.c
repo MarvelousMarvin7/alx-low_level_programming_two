ac546503b857792c5e9c1d48b1b38351afc9fd42
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-10 13:13:55 -0800

    C - Malloc and Free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Contains copy of string of pointer
+ * @str: string
+ * Return: Null if false
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+
+	ptr = (char *) malloc(strlen(str + 1));
+	if (ptr != NULL)
+		strcpy(ptr, str);
+	if (str == NULL)
+		return (0);
+	return (ptr);
+	free(ptr);
+}
