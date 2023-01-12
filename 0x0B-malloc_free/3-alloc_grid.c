4b6157c611dea5872f3924add97a9d77119f532c
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-12 14:08:48 -0800

    C - Malloc and Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,17 +18,20 @@ int **alloc_grid(int width, int height)
 	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = malloc(width * sizeof(int));
 		if (ptr[i] == NULL)
+		{
+			free(ptr[i]);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	return (ptr);
-	free(ptr);
 }
