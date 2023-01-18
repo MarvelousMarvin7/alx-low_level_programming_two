c6d1084aaa3f6e209e58461bdc4c3d07ab017726
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-18 12:14:28 -0800

    C - More Malloc and Free

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: destination
+ * @s2: source
+ * @n: bytes to be allocated
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i, j;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+	if (n >= j)
+		n = j;
+
+	ptr = (char *)malloc(strlen(s2) + n + 1);
+	if (ptr == NULL)
+		return (NULL);
+	if (ptr)
+	{
+		strcpy(ptr, s1);
+		strncat(ptr, s2, n);
+	}
+	return (ptr);
+	free(ptr);
+}
