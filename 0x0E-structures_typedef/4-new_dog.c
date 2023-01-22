9fd4e13cbe59ebdf40ada8ea1d8fd810c2356cb8
MarvelousMarvin7 <marvinagyei3@gmail.com>
2023-01-22 07:17:56 -0800

    C - Structures, Typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,58 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates new dog
+ * @new_dog: - pointer to struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: Owner of dog
+ *
+ * Return: NULL on failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+	int i, j, k;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	/*create space for size of ndog pointer*/
+	ndog = malloc(sizeof(ndog));
+	if (ndog == NULL)
+	{
+		return (NULL);
+		free(ndog);
+	}
+
+	/*assign j and k as name and owner*/
+	for (j = 0; name[j]; j++)
+		;
+	for (k = 0; owner[k]; k++)
+		;
+	/*create space for the input of name and owner*/
+	ndog->name = malloc(sizeof(j + 1));
+
+	ndog->owner = malloc(sizeof(k + 1));
+
+	if (ndog->name == 0 || ndog->owner == 0)
+	{
+		free(ndog->name);
+		free(ndog->owner);
+		free(ndog);
+		return (NULL);
+	}
+
+	for (i = 0; i < j; i++)
+		ndog->name[i] = name[i];
+	ndog->name[i] = '\0';
+
+	ndog->age = age;
+
+	for (i = 0; i < k; i++)
+		ndog->owner[i] = owner[i];
+	ndog->owner[i] = '\0';
+
+	return (ndog);
+}
